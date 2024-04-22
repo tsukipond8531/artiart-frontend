87a0cf144e8523387e12c974bc4ff6219f47b32a
Ahmad Saddique <46298081+AhmedSaddique@users.noreply.github.com>
2024-04-22 16:32:54 +0500

    Latest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
+import CategorySlider from "components/Carousel/Category";
 import MainSlider from "components/Carousel/MainSlider";
 import Product from "components/Home/Product";
-
+import tra1 from "../../public/assets/images/tra1.jpg"
+import tra2 from "../../public/assets/images/tra2.jpg"
+interface Category {
+  id: number;
+  name: string;
+  image: any;
+}
 export default function Home() {
+  const categories: Category[] = [
+    { id: 1, name: 'Category 1', image: tra1 },
+    { id: 2, name: 'Category 2', image: tra2 },
+    { id: 1, name: 'Category 1', image: tra1 },
+    { id: 2, name: 'Category 2', image: tra2 },
+    { id: 1, name: 'Category 1', image: tra1 },
+    { id: 2, name: 'Category 2', image: tra2 },
+    { id: 1, name: 'Category 1', image: tra1 },
+    { id: 2, name: 'Category 2', image: tra2 },
+    { id: 1, name: 'Category 1', image: tra1 },
+    { id: 2, name: 'Category 2', image: tra2 },
+    // Add more categories as needed
+  ];
   return (
    <>
    <MainSlider/>
+   <CategorySlider
+   categories={categories}
+   />
    <Product/>
    </>
   );
---
+++ src/components/Carousel/Category/index.tsx
@@ -0,0 +1,58 @@
+"use client"
+import React from 'react';
+import Image from 'next/image';
+import Container from 'components/Common/Container';
+import { HeadingH4 } from 'components/Common/Heading';
+import { useKeenSlider } from 'keen-slider/react';
+import 'keen-slider/keen-slider.min.css';
+import Link from 'next/link';
+
+interface Category {
+  id: number;
+  name: string;
+  image: string;
+}
+
+interface CategorySliderProps {
+  categories: Category[];
+}
+
+const CategorySlider: React.FC<CategorySliderProps> = ({ categories }) => {
+  const [sliderRef] = useKeenSlider<HTMLDivElement>({
+    breakpoints: {
+      '(min-width: 400px)': {
+        slides: { perView: 2, spacing: 5 },
+      },
+      '(min-width: 1000px)': {
+        slides: { perView: 5, spacing: 10 },
+      },
+    },
+    slides: { perView: 1 },
+  });
+
+  return (
+    <Container>
+      <div ref={sliderRef} className="keen-slider">
+        {categories.map((category) => (
+          <Link href={"/products"} className="keen-slider__slide" key={category.id}>
+            <div className="space-y-2 flex flex-col items-center">
+              <div className="border w-44 h-44 rounded-full">
+                <Image
+                  className="rounded-full w-44 h-44"
+                  width={150}
+                  height={150}
+                  src={category.image}
+                  alt={category.name}
+                />
+              </div>
+              <HeadingH4 title={category.name} />
+            </div>
+          </Link>
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default CategorySlider;
+
