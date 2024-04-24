8faf13efae0042d78f175286708102c8876bc8b0
Ahmad Saddique <46298081+AhmedSaddique@users.noreply.github.com>
2024-04-24 19:01:22 +0500

    Latest filter

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Container from "components/Common/Container";
 
 import art1 from "../../../public/assets/images/art/art1.png";
@@ -19,29 +20,79 @@ import art88 from "../../../public/assets/images/art/art88.jpg";
 import ProductCard from "components/Common/ProductCard";
 import Footer from "components/layout/Footer";
 import Navbar from "components/layout/Header/Navbar";
+import Popoverlist from "components/Common/Popover";
+import { Para14 } from "components/Common/Paragraph";
+import { Checkbox } from 'antd';
+import type { CheckboxProps } from 'antd';
+import Input from "components/Common/Input";
 
 
 export default function Products() {
+  const onChange: CheckboxProps['onChange'] = (e) => {
+  console.log(`checked = ${e.target.checked}`);
+};
   return (
     <>
     <Navbar/>
       <Container>
 
-        <div className="w-[92%] m-auto">
-          <h3 className="text-xl-h3  mt-10 mb-20">Artiart</h3>
+          <h3 className="text-xl font-bold mt-10 mb-20">Artiart</h3>
           {/* filter container */}
-          <div className="flex justify-between flex-wrap mb-10" >
-            <div className="flex gap-10 border-sky-2"  >
-              <p>Filter : </p>
-              <div>Availability </div>
-              <div>Price </div>
+          <div>
+  <div className="hidden md:block">
+    <div className="flex justify-between flex-wrap mb-10">
+      <div className="flex gap-10 border-sky-2">
+        <p>Filter :</p>
+        <Popoverlist
+          title="Availability"
+          content={
+            <div className="space-y-3">
+              <div className="p-2 flex justify-between items-center border-b-2">
+                <Para14 endicon={" selected"} title={"0"} />
+                <div className="underline cursor-pointer">Reset</div>
+              </div>
+              <div>
+                <Checkbox onChange={onChange}><Para14 title={"In stock "} endicon={""}/></Checkbox>
+              </div>
+              <div>
+                <Checkbox onChange={onChange}><Para14 title={"Out of Stock"} endicon={""}/></Checkbox>
+              </div>
             </div>
-            <div className="flex justify-end gap-10" > 
-              <p>Sort by:</p>
-              <div>Best Selling </div>
-              <div className="">20 products </div></div>
-
-          </div>
+          }
+        />
+        <Popoverlist
+          title="Price"
+          content={
+            <div className="space-y-3">
+              <div className="p-2 flex justify-between items-center border-b-2">
+                <Para14 endicon={"200.00"} title={" The highest price is Dhs. "} />
+                <div className="underline cursor-pointer">Reset</div>
+              </div>
+              <div className="flex gap-2">
+                <Input type='number' name='From' placeholder='Enter Price' label='From'/>
+                <Input type='number' name='To' placeholder='Enter Price' label='To'/>
+              </div>
+            </div>
+          }
+        />
+      </div>
+      <div className="flex justify-between gap-10">
+        <p>Sort by:</p>
+        <select className="md:w-28 block w-full border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none">
+          <option selected>Feature</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+        </select>
+        <div className="">20 products</div>
+      </div>
+    </div>
+  </div>
+  <div className="md:hidden  mb-4">
+    <p>Filter</p>
+    <p>Tap here to show filters</p>
+  </div>
+</div>
 
           {/* Cards */}
 
@@ -57,10 +108,6 @@ export default function Products() {
           { image: art8,image2:art88, title: "DUMBO TRAVEL MUG (OUTDOORS)", price: 99,link:"/detail" },
         ]}
         />
-
-
-        </div>
-
 
 
       </Container>
