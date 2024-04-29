6f7c9de523660faae53856ab7269a9e14286b410
Ahmad Saddique <46298081+AhmedSaddique@users.noreply.github.com>
2024-04-29 16:23:03 +0500

    API All Products

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,27 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-      config.module.rules.push({
-        test: /\.mp4$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              publicPath: '/_next/static/videos',
-              outputPath: `${isServer ? '../' : ''}static/videos`,
-              name: '[name].[hash].[ext]',
-            },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.mp4$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/videos',
+            outputPath: `${isServer ? '../' : ''}static/videos`,
+            name: '[name].[hash].[ext]',
           },
-        ],
-      });
-  
-      return config;
-    },
-  };
-  
-  
+        },
+      ],
+    });
+
+    return config;
+  },
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+};
+
 export default nextConfig;
