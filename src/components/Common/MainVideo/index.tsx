86c2c9de370a82d0f33f56b0fa6da23a1351a7d3
monolab825 <p.smiles777@gmail.com>
2024-05-16 16:32:34 +0500

    video

--- src/components/Common/MainVideo/index.tsx
+++ src/components/Common/MainVideo/index.tsx
@@ -5,9 +5,8 @@ import React from 'react'
 const VideoMain = () => {
   return (
     <div>
-<video className='w-full object-cover h-[100%]' controls={false} loop muted  autoPlay >
+<video className='w-full object-cover h-[100%]' controls={false} loop muted  autoPlay playsInline>
       <source src='/assets/images/main.mp4' type="video/mp4" />
-    
     </video>
 
     </div>
