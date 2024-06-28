b5bf74e320972f733cdca2411b74b30d0dae6aae
tsukipond8531 <reader2027@gmail.com>
2024-06-28 16:18:21 +0500

    fixes: Minor styling,buy now button & add shippiing

--- src/components/Common/Input/index.tsx
+++ src/components/Common/Input/index.tsx
@@ -18,12 +18,8 @@ const Input: React.FC<InputProps> = ({ type, name, placeholder, label, onChange,
           type={type}
           name={name}
           id= {id ? id: "hs-floating-input-email"}
-          className="peer p-4 block w-full border rounded-md  border-gray-200  text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none
-            focus:pt-6
-            focus:pb-2
-            [&:not(:placeholder-shown)]:pt-6
-            [&:not(:placeholder-shown)]:pb-2
-            autofill:pt-6
+          className="peer p-4 block w-full border rounded-md  border-gray-200  text-sm placeholder:text-transparent  disabled:opacity-50 disabled:pointer-events-none
+           
             autofill:pb-2"
           placeholder={placeholder}
           onChange={onChange}
@@ -36,11 +32,9 @@ const Input: React.FC<InputProps> = ({ type, name, placeholder, label, onChange,
           className="absolute top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent  origin-[0_0] peer-disabled:opacity-50 peer-disabled:pointer-events-none
             peer-focus:scale-90
             peer-focus:translate-x-0.5
-            peer-focus:-translate-y-1.5
             peer-focus:text-gray-500
             peer-[:not(:placeholder-shown)]:scale-90
             peer-[:not(:placeholder-shown)]:translate-x-0.5
-            peer-[:not(:placeholder-shown)]:-translate-y-1.5
             peer-[:not(:placeholder-shown)]:text-gray-500"
         >
           {label}
